76a60e7eea2e3a46627dd846d628b8ec359660ab
leonardo.dantas <leonardo1317@yahoo.com.br>
2026-06-20 19:17:30 -0300

    refactor: replace manual setters with Lombok @Setter in TransactionContext

--- src/main/java/com/leotech/benefits/authorizer/app/usecases/impl/transaction/TransactionContext.java
+++ src/main/java/com/leotech/benefits/authorizer/app/usecases/impl/transaction/TransactionContext.java
@@ -1,45 +1,33 @@
 package com.leotech.benefits.authorizer.app.usecases.impl.transaction;
 
 import com.leotech.benefits.authorizer.domain.card.Card;
+import com.leotech.benefits.authorizer.domain.shared.CustomException;
 import com.leotech.benefits.authorizer.domain.transaction.Transaction;
 import lombok.Setter;
-
-import com.leotech.benefits.authorizer.domain.shared.CustomException;
 
 public class TransactionContext {
 
     private final Transaction transaction;
     @Setter
     private Card card;
+    @Setter
     private HandlerStatus status = HandlerStatus.CONTINUE;
+    @Setter
     private CustomException exception;
 
     public TransactionContext(final Transaction transaction) {
         this.transaction = transaction;
     }
 
-    public Transaction transaction() {
-        return transaction;
-    }
-
-    public Card card() {
-        return card;
-    }
-
     public HandlerStatus status() {
         return status;
-    }
-
-    public void setStatus(final HandlerStatus status) {
-        this.status = status;
     }
 
     public CustomException exception() {
         return exception;
     }
 
-    public void setException(final CustomException exception) {
-        this.exception = exception;
+    public Transaction transaction() {
+        return transaction;
     }
-
 }
